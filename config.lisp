e65f82051dbba43d3e6143ae56135f74610e6c12
Simen Heggestøyl <simenheg@ifi.uio.no>
2014-03-10 15:28:29 +0100

    Add copyright headers

--- config.lisp
+++ config.lisp
@@ -1,3 +1,20 @@
+;;; config.lisp --- Functions for reading configuration files
+
+;; Copyright (C) 2013-2014 Simen Heggestøyl
+
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (defpackage :config
   (:use :cl :util :alexandria :cl-ppcre)
   (:export :read-config-file))
--- sparql2json.lisp
+++ sparql2json.lisp
@@ -1,5 +1,22 @@
 ;#!/usr/bin/sbcl --script
 
+;;; sparql2json.lisp --- SPARQL endpoint analyzer for PepeSearch
+
+;; Copyright (C) 2013-2014 Simen Heggestøyl
+
+;; This program is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #-quicklisp
 (let ((quicklisp-init-1
        (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname)))
